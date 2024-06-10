debbfa8498a9510d5474c0a335370eedf12d19c8
Alejandro-Moreira <moreiramikel11@gmail.com>
2024-06-09 23:53:41 -0500

    Actualizacion del Login

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -2,11 +2,11 @@
 export const environment = {
   production: false,
   firebaseConfig: {  
-  apiKey: "AIzaSyCJ3vXcjbTnNTExKePABOEGgS8FAxJgEjc",
-  authDomain: "prueba-90813.firebaseapp.com",
-  projectId: "prueba-90813",
-  storageBucket: "prueba-90813.appspot.com",
-  messagingSenderId: "398671273049",
-  appId: "1:398671273049:web:1e81a7572646fe6caca548"
-  },
+      apiKey: "AIzaSyDYLDYSYrPcTCFDUKQY4hqBbtI9T-Ft7sI",
+      authDomain: "login-155ac.firebaseapp.com",
+      projectId: "login-155ac",
+      storageBucket: "login-155ac.appspot.com",
+      messagingSenderId: "111126684895",
+      appId: "1:111126684895:web:f883f20839c8c3a0d4356a"
+    }
 };
