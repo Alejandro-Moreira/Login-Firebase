bf3de4bed772443c570061848566759d7010486c
Alejandro Moreira <117743484+Alejandro-Moreira@users.noreply.github.com>
2024-06-09 23:55:10 -0500

    Update

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -1,12 +1,4 @@
 // src/environments/environment.ts
 export const environment = {
-  production: false,
-  firebaseConfig: {  
-      apiKey: "AIzaSyDYLDYSYrPcTCFDUKQY4hqBbtI9T-Ft7sI",
-      authDomain: "login-155ac.firebaseapp.com",
-      projectId: "login-155ac",
-      storageBucket: "login-155ac.appspot.com",
-      messagingSenderId: "111126684895",
-      appId: "1:111126684895:web:f883f20839c8c3a0d4356a"
-    }
+  production: false
 };
